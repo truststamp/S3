scality/cloudserver

REFACTO: Metadata object using class
Using a class to compute regular object
metadata

lib/services.js
@@ -3,6 +3,8 @@ import assert from 'assert';
 import async from 'async';
 import { errors } from 'arsenal';
 
+import ObjectMD from './metadata/models/object/object';
+
 import BucketInfo from './metadata/BucketInfo';
 import bucketShield from './api/apiUtils/bucket/bucketShield';
 import acl from './metadata/acl';
@@ -169,56 +171,33 @@ export default {
             lastModifiedDate } = params;
         log.trace('storing object in metadata');
         assert.strictEqual(typeof bucketName, 'string');
-        const omVal = {};
-        // Version 2 changes the format of the location property
-        omVal['md-model-version'] = 2;
+        const md = new ObjectMD();
         // If an IAM user uploads a resource,
         // the owner should be the parent account.
         // http://docs.aws.amazon.com/AmazonS3/
         // latest/dev/access-control-overview.html
-        omVal['owner-display-name'] =
-            authInfo.getAccountDisplayName();
+        md.setOwnerDisplayName(authInfo.getAccountDisplayName());
         // This should be object creator's canonical ID.
-        omVal['owner-id'] = authInfo.getCanonicalID();
-        omVal['cache-control'] = cacheControl;
-        omVal['content-disposition'] = contentDisposition;
-        omVal['content-encoding'] = contentEncoding;
-        omVal.expires = expires;
-        omVal['content-length'] = size;
-        omVal['content-type'] = contentType;
+        md.setOwnerId(authInfo.getCanonicalID())
+            .setCacheControl(cacheControl)
+            .setContentDisposition(contentDisposition)
+            .setContentEncoding(contentEncoding)
+            .setExpires(expires)
+            .setContentLength(size)
+            .setContentType(contentType)
+            .setContentMd5(contentMD5)
+            .setLocation(dataGetInfo);
         // Sending in last modified date in object put copy since need
         // to return the exact date in the response
-        omVal['last-modified'] = lastModifiedDate || new Date().toJSON();
-        omVal['content-md5'] = contentMD5;
-
-        omVal['x-amz-server-version-id'] = '';
-        // TODO: Handle this as a utility function for all object puts
-        // similar to normalizing request but after checkAuth so
-        // string to sign is not impacted.  This is GH Issue#89.
-        omVal['x-amz-storage-class'] = 'STANDARD';
-        omVal['x-amz-server-side-encryption'] = '';
-        omVal['x-amz-server-side-encryption-aws-kms-key-id'] = '';
-        omVal['x-amz-server-side-encryption-customer-algorithm'] = '';
-        omVal.location = dataGetInfo;
-
+        if (lastModifiedDate) {
+            md.setLastModified(lastModifiedDate);
+        }
         if (cipherBundle) {
-            omVal['x-amz-server-side-encryption'] = cipherBundle.algorithm;
+            md.setAmzServerSideEncryption(cipherBundle.algorithm);
             if (cipherBundle.masterKeyId) {
-                omVal['x-amz-server-side-encryption-aws-kms-key-id'] =
-                    cipherBundle.masterKeyId;
+                md.setAmzEncryptionKeyId(cipherBundle.masterKeyId);
             }
         }
-
-        // simple/no version. will expand once object versioning is introduced
-        omVal['x-amz-version-id'] = 'null';
-        omVal.acl = {
-            Canned: 'private',
-            FULL_CONTROL: [],
-            WRITE_ACP: [],
-            READ: [],
-            READ_ACP: [],
-        };
-
         // Store user provided metadata.  TODO: limit size.
         // For multipart upload this also serves to transfer
         // over metadata originally sent with the initiation
@@ -228,23 +207,17 @@ export default {
         // will reassign some of the above values with the ones
         // from the intiation of the multipart upload
         // (for instance, storage class)
-        Object.keys(metaHeaders).forEach(val => {
-            omVal[val] = metaHeaders[val];
-        });
-
+        md.setHeaders(metaHeaders);
         if (overrideMetadata) {
-            Object.keys(overrideMetadata).forEach(val => {
-                omVal[val] = overrideMetadata[val];
-            });
+            md.setHeaders(overrideMetadata);
         }
-        log.trace('object metadata', { omVal });
         // If this is not the completion of a multipart upload
         // parse the headers to get the ACL's if any
         if (!multipart) {
             const parseAclParams = {
                 headers,
                 resourceType: 'object',
-                acl: omVal.acl,
+                acl: md.getAcl(),
                 log,
             };
             log.trace('parsing acl from headers');
@@ -253,8 +226,11 @@ export default {
                     log.warn('error parsing acl', { error: err });
                     return cb(err);
                 }
-                omVal.acl = parsedACL;
-                metadata.putObjectMD(bucketName, objectKey, omVal, log, err => {
+                md.setAcl(parsedACL);
+                log.trace('object metadata', {
+                    omVal: md.getData(),
+                });
+                metadata.putObjectMD(bucketName, objectKey, md, log, err => {
                     if (err) {
                         log.error('error from metadata', { error: err });
                         return cb(err);
@@ -265,7 +241,10 @@ export default {
                 return undefined;
             });
         } else {
-            metadata.putObjectMD(bucketName, objectKey, omVal, log, err => {
+            log.trace('object metadata', {
+                omVal: md.getData(),
+            });
+            metadata.putObjectMD(bucketName, objectKey, md, log, err => {
                 if (err) {
                     log.error('error from metadata', { error: err });
                     return cb(err);

truststamp/S3

lib/services.js
@@ -3,6 +3,8 @@ import assert from 'assert';
 import async from 'async';
 import { errors } from 'arsenal';
 
+import ObjectMD from './metadata/models/object/object';
+
 import BucketInfo from './metadata/BucketInfo';
 import bucketShield from './api/apiUtils/bucket/bucketShield';
 import acl from './metadata/acl';
@@ -169,56 +171,33 @@ export default {
             lastModifiedDate } = params;
         log.trace('storing object in metadata');
         assert.strictEqual(typeof bucketName, 'string');
-        const omVal = {};
-        // Version 2 changes the format of the location property
-        omVal['md-model-version'] = 2;
+        const md = new ObjectMD();
         // If an IAM user uploads a resource,
         // the owner should be the parent account.
         // http://docs.aws.amazon.com/AmazonS3/
         // latest/dev/access-control-overview.html
-        omVal['owner-display-name'] =
-            authInfo.getAccountDisplayName();
+        md.setOwnerDisplayName(authInfo.getAccountDisplayName());
         // This should be object creator's canonical ID.
-        omVal['owner-id'] = authInfo.getCanonicalID();
-        omVal['cache-control'] = cacheControl;
-        omVal['content-disposition'] = contentDisposition;
-        omVal['content-encoding'] = contentEncoding;
-        omVal.expires = expires;
-        omVal['content-length'] = size;
-        omVal['content-type'] = contentType;
+        md.setOwnerId(authInfo.getCanonicalID())
+            .setCacheControl(cacheControl)
+            .setContentDisposition(contentDisposition)
+            .setContentEncoding(contentEncoding)
+            .setExpires(expires)
+            .setContentLength(size)
+            .setContentType(contentType)
+            .setContentMd5(contentMD5)
+            .setLocation(dataGetInfo);
         // Sending in last modified date in object put copy since need
         // to return the exact date in the response
-        omVal['last-modified'] = lastModifiedDate || new Date().toJSON();
-        omVal['content-md5'] = contentMD5;
-
-        omVal['x-amz-server-version-id'] = '';
-        // TODO: Handle this as a utility function for all object puts
-        // similar to normalizing request but after checkAuth so
-        // string to sign is not impacted.  This is GH Issue#89.
-        omVal['x-amz-storage-class'] = 'STANDARD';
-        omVal['x-amz-server-side-encryption'] = '';
-        omVal['x-amz-server-side-encryption-aws-kms-key-id'] = '';
-        omVal['x-amz-server-side-encryption-customer-algorithm'] = '';
-        omVal.location = dataGetInfo;
-
+        if (lastModifiedDate) {
+            md.setLastModified(lastModifiedDate);
+        }
         if (cipherBundle) {
-            omVal['x-amz-server-side-encryption'] = cipherBundle.algorithm;
+            md.setAmzServerSideEncryption(cipherBundle.algorithm);
             if (cipherBundle.masterKeyId) {
-                omVal['x-amz-server-side-encryption-aws-kms-key-id'] =
-                    cipherBundle.masterKeyId;
+                md.setAmzEncryptionKeyId(cipherBundle.masterKeyId);
             }
         }
-
-        // simple/no version. will expand once object versioning is introduced
-        omVal['x-amz-version-id'] = 'null';
-        omVal.acl = {
-            Canned: 'private',
-            FULL_CONTROL: [],
-            WRITE_ACP: [],
-            READ: [],
-            READ_ACP: [],
-        };
-
         // Store user provided metadata.  TODO: limit size.
         // For multipart upload this also serves to transfer
         // over metadata originally sent with the initiation
@@ -228,23 +207,17 @@ export default {
         // will reassign some of the above values with the ones
         // from the intiation of the multipart upload
         // (for instance, storage class)
-        Object.keys(metaHeaders).forEach(val => {
-            omVal[val] = metaHeaders[val];
-        });
-
+        md.setUserMetadata(metaHeaders);
         if (overrideMetadata) {
-            Object.keys(overrideMetadata).forEach(val => {
-                omVal[val] = overrideMetadata[val];
-            });
+            md.setUserMetadata(overrideMetadata);
         }
-        log.trace('object metadata', { omVal });
         // If this is not the completion of a multipart upload
         // parse the headers to get the ACL's if any
         if (!multipart) {
             const parseAclParams = {
                 headers,
                 resourceType: 'object',
-                acl: omVal.acl,
+                acl: md.getAcl(),
                 log,
             };
             log.trace('parsing acl from headers');
@@ -253,8 +226,11 @@ export default {
                     log.warn('error parsing acl', { error: err });
                     return cb(err);
                 }
-                omVal.acl = parsedACL;
-                metadata.putObjectMD(bucketName, objectKey, omVal, log, err => {
+                md.setAcl(parsedACL);
+                log.trace('object metadata', {
+                    omVal: md.getAcl(),
+                });
+                metadata.putObjectMD(bucketName, objectKey, md, log, err => {
                     if (err) {
                         log.error('error from metadata', { error: err });
                         return cb(err);
@@ -265,7 +241,10 @@ export default {
                 return undefined;
             });
         } else {
-            metadata.putObjectMD(bucketName, objectKey, omVal, log, err => {
+            log.trace('object metadata', {
+                omVal: md.getAcl(),
+            });
+            metadata.putObjectMD(bucketName, objectKey, md, log, err => {
                 if (err) {
                     log.error('error from metadata', { error: err });
                     return cb(err);